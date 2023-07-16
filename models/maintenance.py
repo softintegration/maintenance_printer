2fb2752d7e99275a83c5b2df7a2e4429067a1e7e
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-16 11:48:19 +0100

    Equipment:set the fields «Front number of Colors» and «Back number of Colors» as required in the case of Both sides equipment

--- models/maintenance.py
+++ models/maintenance.py
@@ -5,7 +5,7 @@ import ast
 from datetime import date, datetime, timedelta
 
 from odoo import api, fields, models, SUPERUSER_ID, _
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 
@@ -17,4 +17,12 @@ class MaintenanceEquipment(models.Model):
     both_sides = fields.Boolean(string='Both sides', default=False)
     front_color_cpt = fields.Integer(string='Front number of Colors')
     back_color_cpt = fields.Integer(string='Back number of Colors')
+
+
+    @api.constrains('both_sides','front_color_cpt','back_color_cpt')
+    def _check_both_sides_color_cpt(self):
+        for each in self:
+            if each.both_sides and (not each.front_color_cpt or not each.back_color_cpt):
+                raise ValidationError(_("Front number of Colors and Back number of Colors should be strictly positive in the case of both sides equipment"))
+
 
